474b89d08c4f3ca86b83d85784416d736f47a684
Олександр Карпюк <sashakarpjuk43@gmail.com>
2024-09-23 15:29:56 +0300

    Added EntityFramework sqlite and design packages

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651533a80a152f92706e36b4313ae68af7ea88f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5830545bc16ccbcd0ea17042ab93b1a2398c2881")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
